c61a420da45268bf4d5a70ea001de29795319918
HumanCity <kukuciro@gmail.com>
2022-08-15 12:16:02 +0700

    commit pertama

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,7 +21,6 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-$routes->setAutoRoute(true);
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
@@ -36,12 +35,10 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+$routes->get('/komik/(:segment)', 'Komik::detail/$1');
 
 $routes->get('/', 'Pages::index');
 
-$routes->get('/komik/create', 'komik::create');
-
-$routes->get('/komik/(:segment)', 'Komik::detail/$1');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
